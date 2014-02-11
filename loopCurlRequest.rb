1be2ab9a43f68f793cc639ece3592c9c283be89e
Salvatore D'Agostino <toto19@gmail.com>
2014-02-11 16:36:02 -0500

    initial import of simple http request script

---
+++ loopCurlRequest.rb
@@ -0,0 +1,21 @@
+require 'net/http'
+require 'pp'
+
+numChildren = ARGV[1]
+
+numChildren.to_i.times do |i|
+    pid = fork do
+        puts "Process #{i} started"
+        begin
+            url = URI.parse(ARGV[0])
+            req = Net::HTTP::Get.new(url.to_s)
+            res = Net::HTTP::start(url.host, url.port) {|http|
+                http.request(req)
+            }
+        end while res.code == "200"
+        puts "Process #{i} ended"
+        exit
+    end
+end
+puts "All done"
+exit
